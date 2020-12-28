1f235189bae9964aae4d3c56478fc90a22390c5e
Donma Hsu <no2don@gmail.com>
2020-12-28 12:39:42 +0800

    commit

--- JiebaPrac/Program.cs
+++ JiebaPrac/Program.cs
@@ -9,24 +9,36 @@ namespace JiebaPrac
         {
             Console.WriteLine("Hello Jiqba!");
 
+            var sourceString = "許當麻緩緩的唱著:那樣的回憶那麼足夠足夠我天天都品嚐著寂寞";
+
             JiebaNet.Segmenter.ConfigManager.ConfigFileBaseDir = AppDomain.CurrentDomain.BaseDirectory + @"jiebanet_config";
 
 
             var segmenter = new JiebaNet.Segmenter.JiebaSegmenter();
 
-            var segments = segmenter.CutForSearch("許當麻緩緩的唱著:那樣的回憶那麼足夠足夠我天天都品嚐著寂寞");
-            Console.WriteLine(string.Join(" /  ", segments));
 
-            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Cut -\r\n");
+
+            var segmentsCut = segmenter.Cut(sourceString);
+            Console.WriteLine(string.Join("/", segmentsCut));
+
+
+            Console.WriteLine("\r\n--------------------------------\r\n");
 
             var posSeg = new JiebaNet.Segmenter.PosSeg.PosSegmenter(segmenter);
 
 
-            var s = "許當麻緩緩的唱著:那樣的回憶那麼足夠足夠我天天都品嚐著寂寞";
+            var tokens = posSeg.Cut(sourceString);
+            foreach (var token in tokens) {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(token.Word);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("/");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(token.Flag);
+            }
 
-            var tokens = posSeg.Cut(s);
-
-            Console.WriteLine(string.Join(" ", tokens.Select(token => token.Word+"/"+token.Flag)));
+            Console.ReadLine();
         }
     }
 }
